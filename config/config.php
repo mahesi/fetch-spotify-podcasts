42041afb11b948aeddc126b4e4f7273d2f85c5cd
Marc Siewert <marc@siewert.info>
2021-12-23 11:53:23 +0100

    refactoring config

--- config/config.php
+++ config/config.php
@@ -5,7 +5,9 @@ return [
 	'api-url'                    => 'https://chartable.com/charts/spotify/de',
 
 	// Header for fetching with HTTP
-	'http-header'                => 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0',
+	'http-header'                => [
+		'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0',
+	],
 
 	// URL selector for chartable category links
 	'regex-chartable-categories' => '/\/charts\/spotify\/germany-[-a-zA-Z]+$/',
